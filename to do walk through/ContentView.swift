78897a631fc6f62b83b143eb47824a8e2b0a6058
Farida Balogun <faridabalogun1217@gmail.com>
2023-06-14 16:19:05 +0100

    Initial Commit

---
+++ to do walk through/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  to do walk through
+//
+//  Created by Farida Balogun on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to do walk through/to_do_walk_throughApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_walk_throughApp.swift
+//  to do walk through
+//
+//  Created by Farida Balogun on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_walk_throughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
